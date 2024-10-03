273db94392acf44a5b1aa786f42beca7aee40226
Al Stone <astone@heckgrammar.co.uk>
2024-10-03 15:15:52 +0100

    Add project files.

---
+++ ProgrammingConcepts12B24/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Reflection.Metadata;
+
+namespace ProgrammingConcepts12B24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //variable declaration
+            int aNumber = 3;
+            //constant declaration
+            const string myConst = "Can't change";
+            //assignment
+            aNumber = 12;
+            string aString = Console.ReadLine();
+
+            //iteration
+            for(int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
+            //selection
+            //subroutine(procedure / function).
+        }
+    }
+}
